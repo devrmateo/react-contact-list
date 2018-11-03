8634e24e2ae5d0d947f0e44d6636728018cbfc83
Matthew Smith <mateoelcule@gmail.com>
2018-11-02 17:37:28 -0700

    Installed 'escape-string-regexp' and 'sort-by' packages and added functionality to search the contact list for specific names and to sort them.

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
 
 class ListContacts extends Component {
@@ -20,8 +22,19 @@ class ListContacts extends Component {
      }
 
      render() {
-          return (
+          let showingContacts;
 
+          if (this.state.query) {
+               const match = new RegExp(escapeRegExp(this.state.query), 'i');
+               showingContacts = this.props.contacts.filter((contact) => match.test(contact.name))
+          } else {
+               showingContacts = this.props.contacts;
+          }
+
+          showingContacts.sort(sortBy('name'));
+
+
+          return (
                <div className="list-contacts">
                     <input
                          className="search-contacts"
@@ -31,7 +44,7 @@ class ListContacts extends Component {
                          onChange={(event) => this.updateQuery(event.target.value)}
                     />
                     <ol className="contact-list">
-                    {this.props.contacts.map((contact) => {
+                    {showingContacts.map((contact) => {
                          return (
                               <li key={contact.id} className="contact-list-item">
                                    <div className="contact-avatar" style=
