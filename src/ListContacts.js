bda9a8a16aefe5c9a8d070107ff546591eb95567
Matthew Smith <mateoelcule@gmail.com>
2018-11-02 17:00:47 -0700

    Intalled prop-types package and rewrote 'ListContacts' function component as a class, with the search query being controlled in its state.

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,31 +1,60 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-function ListContacts (props) {
-return (
-     <ol className="contact-list">
-          {props.contacts.map((contact) => {
-               return (
-                    <li key={contact.id} className="contact-list-item">
-                         <div className="contact-avatar" style=
-                         {{
-                              backgroundImage: `url(${contact.avatarURL})`
-                         }} />
-                         <div className="contact-details">
-                              <p>{contact.name}</p>
-                              <p>{contact.email}</p>
-                         </div>
-                         <button
-                              className="contact-remove"
-                              onClick={() => props.onDeleteContact(contact)}>
-                              Remove
-                         </button>
-                    </li>
-                    )
-               }
-          )}
-     </ol>
 
-)
+class ListContacts extends Component {
+     static propTypes = {
+          contacts: PropTypes.array.isRequired,
+          onDeleteContact: PropTypes.func.isRequired
+
+     }
+
+     state = {
+          query: ''
+     }
+
+     updateQuery = (query) => {
+           this.setState({
+               query: query.trim()
+          })
+     }
+
+     render() {
+          return (
+
+               <div className="list-contacts">
+                    <input
+                         className="search-contacts"
+                         type="text"
+                         placeholder="Search contacts"
+                         value={this.state.query}
+                         onChange={(event) => this.updateQuery(event.target.value)}
+                    />
+                    <ol className="contact-list">
+                    {this.props.contacts.map((contact) => {
+                         return (
+                              <li key={contact.id} className="contact-list-item">
+                                   <div className="contact-avatar" style=
+                                   {{
+                                        backgroundImage: `url(${contact.avatarURL})`
+                                   }} />
+                                   <div className="contact-details">
+                                        <p>{contact.name}</p>
+                                        <p>{contact.email}</p>
+                                   </div>
+                                   <button
+                                        className="contact-remove"
+                                        onClick={() => this.props.onDeleteContact(contact)}>
+                                        Remove
+                                   </button>
+                              </li>
+                              )
+                         }
+                    )}
+               </ol>
+               </div>
+          )
+     }
 }
 
 
